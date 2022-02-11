979dcf1b81d7762227c8d5ed9f38bd51c46716e5
ezito developer <33319787+ezitoir@users.noreply.github.com>
2022-02-10 22:26:38 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 if(!Object.prototype.includeKeys){
     Object.defineProperty( Object.prototype , 'includeKeys' , {
-        value : function ( keys = Array | String  ){
+        value : function ( keys = null | Array | String  , callback = null | Function ){
             if(!(keys instanceof Array ) && !(typeof keys === 'string')) throw { ErrorType : "param type erorr" , message : "keys nust instanceof Array!" };
-            keys = typeof keys === 'string' ? keys.split(" ") : keys ;
+            keys = (typeof keys === 'string' ? keys.split(" ") : keys ).filter(function (params) {
+                return params.trim() !=='';
+            });
             let context = this;
 
             let result = keys.map(function(key){
@@ -17,7 +19,12 @@ if(!Object.prototype.includeKeys){
                 }
                 return 0;
             });
-            return result.length === keys.length ? result.pop() : false ;
+            if(result.length === keys.length){ 
+                if( Object.toString(callback) === '[object Function]' || typeof callback === 'function')
+                return callback.call( this , result.pop())
+                return result.pop();
+            }
+
         } ,
         writable : false,
         configurable : false ,

de026f19dcee12d8b096e78563ed038f37fe020d
ezito developer <33319787+ezitoir@users.noreply.github.com>
2022-02-11 06:19:41 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 if(!Object.prototype.includeKeys){
     Object.defineProperty( Object.prototype , 'includeKeys' , {
         value : function ( keys = null | Array | String  , callback = null | Function ){
+
+            // check type for keys accept Array list of key or string key 
             if(!(keys instanceof Array ) && !(typeof keys === 'string')) throw { ErrorType : "param type erorr" , message : "keys nust instanceof Array!" };
+
+    
             keys = (typeof keys === 'string' ? keys.split(" ") : keys ).filter(function (params) {
                 return params.trim() !=='';
             });
